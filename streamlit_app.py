54b665a585d511ea3c59d6ce7892f21a9ce78370
GEETHESWARI <121534207+GEETHESWARI@users.noreply.github.com>
2024-09-23 18:58:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import plotly.express as px
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
 
 st.title(' 🌦 Weather Prediction ⛈')
 
 st.info(' This Machine learning Model predicts the weather')
+
+
+weather=pd.read_csv("https://github.com/GEETHESWARI/Weather_pred/blob/main/seattle-weather.csv")
+
+weather
