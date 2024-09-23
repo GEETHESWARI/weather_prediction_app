c070c7fed58c966e0cb1718faa6974c3bcf76e2d
GEETHESWARI <121534207+GEETHESWARI@users.noreply.github.com>
2024-09-23 18:49:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title(' 🌦️ Weather Prediction ⛈️')
 
-st.info('This Machine learning Model predicts the weather')
+st.info(' This Machine learning Model predicts the weather')
