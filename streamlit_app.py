e427a1edae934d0c9373dd0404a430dc368e7f54
GEETHESWARI <121534207+GEETHESWARI@users.noreply.github.com>
2024-09-25 11:21:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+from sklearn.ensemble import RandomForestClassifier
 
 
 st.title(' 🌦 Weather Prediction ⛈')
